48dbcd64834dd8802a6c54102d60f3061ed9fde6
SeungMin <sonsean89@gmail.com>
2022-09-27 17:42:09 +0900

    v4

--- scripts/move_function.py
+++ scripts/move_function.py
@@ -22,10 +22,16 @@ class MoveTurtle:
             rospy.loginfo("원을 그리는중")
             self.cmd2()
         
-    def cmd(self):
+    def cmd(self): #사각형 & 삼각형
         pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
         rate = rospy.Rate(5)
         while not rospy.is_shutdown():
+            if rospy.has_param("turtlemove_client_node/square_size"):
+                self.lin_vel = rospy.get_param("turtlemove_client_node/square_size")
+            
+            elif rospy.has_param("turtlemove_client_node/triangle_size"):
+                self.lin_vel = rospy.get_param("turtlemove_client_node/triangle_size")
+
             cmd_msg = Twist()
             cmd_msg.linear.x = self.lin_vel 
             cmd_msg.linear.y = 0
@@ -40,11 +46,15 @@ class MoveTurtle:
             for i in range(10):
                 pub.publish(cmd_msg)
                 rate.sleep()
-                
-    def cmd2(self):
+
+    def cmd2(self): #원
         pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
         rate = rospy.Rate(5)
         while not rospy.is_shutdown():
+            if rospy.has_param("turtlemove_client_node/circle_size") and rospy.has_param("turtlemove_client_node/angle_vel"):
+                self.lin_vel = rospy.get_param("turtlemove_client_node/circle_size")
+                self.ang_vel = rospy.get_param("turtlemove_client_node/angle_vel")
+
             cmd_msg = Twist()
             cmd_msg.linear.x = self.lin_vel 
             cmd_msg.linear.y = 0
--- scripts/turtle_move_client_node.py
+++ scripts/turtle_move_client_node.py
@@ -19,7 +19,7 @@ def usage():
     return "%s [x y z]"%sys.argv[0]
 
 
-if __name__ == "__main__":
+""" if __name__ == "__main__":
     mode = input("모드를 입력하시오: ")
     if mode == "square":
         distance = float(input("거리를 입력하시오: "))
@@ -32,6 +32,24 @@ if __name__ == "__main__":
     elif mode == "circle":
         distance = float(input("Lin_vel을 입력하시오: "))
         angle = float(input("Ang_vel을 입력하시오: "))
+    else:
+        mode,distance,angle = 0
+
+    move_turtle_client(mode, distance, angle)
+ """
+if __name__ == "__main__":
+    mode = input("모드를 입력하시오: ")
+    if mode == "square":
+        distance = rospy.get_param('turtlemove_client_node/square_size')
+        angle = 0
+
+    elif mode == "triangle":
+        distance = rospy.get_param('turtlemove_client_node/triangle_size')
+        angle = 0
+
+    elif mode == "circle":
+        distance = rospy.get_param('turtlemove_client_node/circle_size')
+        angle = rospy.get_param('turtlemove_client_node/angle_vel')
     else:
         mode,distance,angle = 0
 
